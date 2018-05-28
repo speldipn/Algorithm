57a3173684486c31f29c7ef0dc60a15623bfda00
speldipn <speldipn.gmail.com>
2018-05-28 17:41:12 +0900

    add problem median of two sorted arrays

---
+++ src/leetcode/LongestSubstring.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class LongestSubstring {
+  public static void main(String[] args) {
+    if (lengthOfLongestSubstring("abcabcbb") != 3) {
+      System.out.println("incorrect problem1");
+    }
+
+    if (lengthOfLongestSubstring("bbbbb") != 1) {
+      System.out.println("incorrect problem2");
+    }
+
+    if (lengthOfLongestSubstring("pwwkew") != 3) {
+      System.out.println("incorrect problem3");
+    }
+  }
+
+  public static int lengthOfLongestSubstring(String s) {
+    int maxLen = 0;
+    String temp = "";
+
+
+
+
+    return maxLen;
+  }
+}
