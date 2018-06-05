6f093e08d2faa167e8f87d9356a395e4c15379f9
Oh, Joon young <speldipn@gmail.com>
2018-06-06 00:45:31 +0900

    cleanup code

--- src/Algo2.java
+++ src/Algo2.java
@@ -4,8 +4,8 @@ public class Algo2 {
 
   public static void main(String[] args) {
     // sum
-    printSum1(2100000000L);
     printSum(100);
+    printSum1(2100000000L);
     printSum1(100);
     printSum2(1L, 2100000000L);
     printSum2(1L, 100L);
@@ -64,24 +64,19 @@ public class Algo2 {
   public static void solve2(String a, String b) {
     int result = 0;
     int result1 = 0;
-
     long start = System.currentTimeMillis();
-
     String src = a.trim();
     String target = b.trim();
-
     for(int i = 0; i < src.length(); ++i) {
       if(src.charAt(i) == ' ') {
         src = src.substring(0, i) + src.substring(i+1, src.length());
       }
     }
-
     for(int i = 0; i < target.length(); ++i) {
       if(target.charAt(i) == ' ') {
         target = target.substring(0, i) + target.substring(i+1, target.length());
       }
     }
-
     if(src.length() != target.length()) {
       System.out.println("아나그램이 아닙니다.");
       return;
@@ -94,13 +89,11 @@ public class Algo2 {
         }
       }
     }
-
     if(target.length() <= 0) {
       System.out.println("아나그램");
     } else {
       System.out.println("아나그램이 아닙니다.");
     }
-
     long end = System.currentTimeMillis();
     System.out.println("Elapes: " + (end-start) + "[ms]");
   }
