485f5fe7d500e46e363660ffedf8c91cbddf63f3
Oh, Joon young <speldipn@gmail.com>
2018-09-10 02:12:57 +0900

    modify Cocktail(Q5)

--- src/data_struct/ch06_sorting/Q5.java
+++ src/data_struct/ch06_sorting/Q5.java
@@ -1,7 +1,5 @@
 package data_struct.ch06_sorting;
 
-import java.util.Arrays;
-
 /**
  * @Author: Oh, Joon young (speldipn)
  * @Since: 2018-09-08
@@ -10,25 +8,7 @@ public class Q5 {
   private static int compareCnt = 0;
   private static int changeCnt = 0;
 
-  private static void __bubbleSort(int a[], int n) {
-    int k = 0;
-    while (k < n - 1) {
-      int last = n - 1;
-      for (int j = n - 1; j > k; --j) {
-        ++compareCnt;
-        if (a[j - 1] > a[j]) {
-          swap(a, j - 1, j);
-          last = j;
-          ++changeCnt;
-        }
-      }
-      k = last;
-    }
-    System.out.println("비교횟수: " + compareCnt);
-    System.out.println("교환횟수: " + changeCnt);
-  }
-
-  private static void bubbleSort(int[] a, int n) {
+  public static void cocktailSort(int[] a, int n) {
     int start = 1;
     int end = n;
     boolean bReverse = false;
@@ -70,14 +50,14 @@ public class Q5 {
     a[j] = temp;
   }
 
-  private static void showArray(int a[]) {
+  public static void showArray(int a[]) {
     for (int i = 0; i < a.length; ++i) {
       System.out.print(a[i] + " ");
     }
     System.out.print("  ");
   }
 
-  private static void shwoArrayLF(int a[]) {
+  public static void showArrayLF(int a[]) {
     for (int i = 0; i < a.length; ++i) {
       System.out.print(a[i] + " ");
     }
@@ -86,8 +66,9 @@ public class Q5 {
 
   public static void main(String[] args) {
     int sample[] = {5, 1, 4, 2, 8, 0, 2};
-    shwoArrayLF(sample);
-    bubbleSort(sample, sample.length);
-    shwoArrayLF(sample);
+    Q5 ob = new Q5();
+    ob.showArrayLF(sample);
+    ob.cocktailSort(sample, sample.length);
+    ob.showArrayLF(sample);
   }
 }
